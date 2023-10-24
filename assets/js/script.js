9d951788554998afde7687a97960e152a54e1ea1
Coder4916 <>
2023-10-24 18:29:28 +0000

    Add code to increment scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,9 +45,11 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right!  :D");
+        incrementScore();
     }
     else {
         alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1])
 }
@@ -69,10 +71,15 @@ function calculateCorrectAnswer() {
 
 function incrementScore() {
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+
 }
 
 function incrementWrongAnswer() {
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
